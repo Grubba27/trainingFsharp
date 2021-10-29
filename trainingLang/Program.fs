e44eaf82e9cd41b60a788bc86b93f4cf14643c16
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-10-29 19:05:39 -0300

    Created mutable

--- trainingLang/Program.fs
+++ trainingLang/Program.fs
@@ -1,4 +1,8 @@
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    let mutable person = "Person"
+    if argv.Length > 0 then
+        // <- é igual ao = só que para mutable
+        person <- argv.[0]
+    printfn "Simple print line %s" person
     0 // return an integer exit code
